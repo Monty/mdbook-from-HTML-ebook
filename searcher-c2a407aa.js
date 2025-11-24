befe472fdf7d3638a2ef4f8f44abc70c15397aa8
Monty <Monty@users.noreply.github.com>
2025-11-24 19:11:49 +0000

    deploy: 33990f9664199b7a896dced37538c56ca6ac8e29

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-8692fb47.js',
+                path_to_root + 'searchindex-59dcaa77.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
